9e5b7bdfde8ddcdcf4b5c7ffd6bd1a3f95f5f086
Youngjae Choi <cdw0807@naver.com>
2021-12-08 18:19:31 +0900

    12/8 signup

---
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -0,0 +1,30 @@
+package com.cos.photogramstart.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller // 1. IoC 2. 파일을 리턴하는 컨트롤러
+public class AuthController {
+    
+    @GetMapping("/auth/signin")
+    public String signinForm() {
+
+        return "auth/signin";
+    }
+
+    @GetMapping("/auth/signup")
+    public String signupForm() {
+
+        return "auth/signup";
+    }
+
+    @PostMapping("/auth/signup")
+    public String signup() {
+
+        return "auth/signin";
+    }
+    
+}
